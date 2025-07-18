053dc55a70c26a80469b68c7f5b517461d28c86e
GABRIEL-ONYANGO <143998221+GABRIEL-ONYANGO@users.noreply.github.com>
2025-07-18 10:13:18 +0300

    Update integration.tests.js

--- server/tests/integration.tests.js
+++ server/tests/integration.tests.js
@@ -1,1 +1,9 @@
+const request = require('supertest');
+const app = require('../app');
 
+describe('POST /api/bugs', () => {
+  it('should create a new bug', async () => {
+    const res = await request(app).post('/api/bugs').send({ title: 'Bug', description: 'Details' });
+    expect(res.statusCode).toBe(201);
+  });
+});
