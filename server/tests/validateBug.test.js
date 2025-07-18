90db52adce77fbb006ddac2a785208ed3e82393b
GABRIEL-ONYANGO <143998221+GABRIEL-ONYANGO@users.noreply.github.com>
2025-07-18 10:11:07 +0300

    Update validateBug.test.js

--- server/tests/validateBug.test.js
+++ server/tests/validateBug.test.js
@@ -1,1 +1,5 @@
-
+// validateBug.test.js
+const validateBug = require('./validateBug');
+test('valid bug returns true', () => {
+  expect(validateBug({ title: 'Crash', description: 'App crashes' })).toBeTruthy();
+});
