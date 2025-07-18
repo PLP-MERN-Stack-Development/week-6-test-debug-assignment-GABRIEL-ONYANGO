60ed21511c715910c43590617a61f81ed8b1a32b
GABRIEL-ONYANGO <143998221+GABRIEL-ONYANGO@users.noreply.github.com>
2025-07-18 10:19:15 +0300

    unit.tests.jsx

--- client/src/tests/unit/unit.tests.jsx
+++ client/src/tests/unit/unit.tests.jsx
@@ -1,1 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import BugForm from './BugForm';
 
+test('renders form inputs', () => {
+  render(<BugForm />);
+  expect(screen.getByLabelText(/title/i)).toBeInTheDocument();
+});
